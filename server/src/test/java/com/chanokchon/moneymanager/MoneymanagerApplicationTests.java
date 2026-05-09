2ab744e46b32ed21bef6cc971192a7cd905f6826
Byte Chanokchon <byte.chanokchon.won@gmail.com>
2026-05-09 16:32:40 +0700

    initial server source

---
+++ server/src/test/java/com/chanokchon/moneymanager/MoneymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanokchon.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
